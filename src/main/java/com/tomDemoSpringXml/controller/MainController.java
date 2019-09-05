76923f1fa46a21c7cd85c72f2f41a49143353533
吳旻璁 <tt920603@yahoo.com.tw>
2019-09-05 17:14:19 +0800

    2019-09-05變更 Author   :  80005458 Content : 1.調整url映射方案

--- src/main/java/com/tomDemoSpringXml/controller/MainController.java
+++ src/main/java/com/tomDemoSpringXml/controller/MainController.java
@@ -3,32 +3,37 @@ package com.tomDemoSpringXml.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import com.tomDemoSpringXml.service.MainServicebyName;
+import com.tomDemoSpringXml.service.MainServicebyType;
+import com.tomDemoSpringXml.service.impl.MainServicebyNameImpl;
 
 //@Controller
 //@RequestMapping("mainController")
 public class MainController extends MultiActionController{
+	private static final Logger LOGGER = LoggerFactory.getLogger(MultiActionController.class);
 	
 	MainServicebyName mainServicebyName;
-	
+//	MainServicebyType mainServicebyType111;
 	/**
 	 * @see 範例入口測試用
-	 * @return Map
+	 * @return ModelAndView
 	 */
-//	@RequestMapping(value="mainActionUsingTest", method = RequestMethod.GET)
-//	@ResponseBody
-//	public Map<String, Object> mainActionUsingTest(){
-//		Map<String, Object> map = new HashMap<String, Object>();
-//		mainServicebyName.mainFunction();
-//
-//		
-//		map.put("responseType", "測試完成");
-//		return map;	
-//	}
-
+	public ModelAndView doFirst(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		LOGGER.info("執行 doFirst");
+		mainServicebyName.mainFunction();
+//		mainServicebyType111.mainFunction();
+		return new ModelAndView("welcome", "responseType","執行 doFirst");		
+	}
+		
+	public ModelAndView doSecond(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		LOGGER.info("執行 doSecond");		
+		return new ModelAndView("welcome", "responseType","執行 doSecond");			
+	}
 	public MainServicebyName getMainServicebyName() {
 		return mainServicebyName;
 	}
@@ -36,20 +41,12 @@ public class MainController extends MultiActionController{
 	public void setMainServicebyName(MainServicebyName mainServicebyName) {
 		this.mainServicebyName = mainServicebyName;
 	}
-	
-	public ModelAndView home(HttpServletRequest request,
-		HttpServletResponse response) throws Exception {
-		ModelAndView model = new ModelAndView("welcome");
-		model.addObject("message", "Home");
-		mainServicebyName.mainFunction();
-		return model;
-	}
 
-	public ModelAndView add(HttpServletRequest request,
-		HttpServletResponse response) throws Exception {
-		ModelAndView model = new ModelAndView("welcome");
-		model.addObject("message", "Add");
-		mainServicebyName.mainFunction();
-		return model;
-	}
+//	public MainServicebyType getMainServicebyType() {
+//		return mainServicebyType;
+//	}
+//
+//	public void setMainServicebyType(MainServicebyType mainServicebyType) {
+//		this.mainServicebyType = mainServicebyType;
+//	}
 }
--- src/main/java/com/tomDemoSpringXml/service/impl/MainServicebyTypeImpl.java
+++ src/main/java/com/tomDemoSpringXml/service/impl/MainServicebyTypeImpl.java
@@ -5,9 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.tomDemoSpringXml.service.MainServicebyName;
+import com.tomDemoSpringXml.service.MainServicebyType;
 
 //@Service
-public class MainServicebyTypeImpl implements MainServicebyName{
+public class MainServicebyTypeImpl implements MainServicebyType{
 	private static final Logger LOGGER = LoggerFactory.getLogger(MainServicebyTypeImpl.class);	
 	
 	@Override
